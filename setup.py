e7c692be70d93ddd9f166606a4a1fa34db83cc04
Anuja Solanki <anuja.solanki9597@gmail.com>
2023-04-27 21:54:01 +0100

    css changes

---
+++ setup.py
@@ -0,0 +1,22 @@
+
+from setuptools import setup
+
+setup(
+    name='cpppackageAnuja',
+    version='1.0.0',
+    description='A short description of my package',
+    packages=['string_conversion'],
+    install_requires=[
+        'requests>=2.25.1',
+        'numpy>=1.19.5',
+       
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    url='https://github.com/my_username/my_package',
+    author='Anuja',
+    author_email='anuja.solanki9597@gmail.com'
+)
